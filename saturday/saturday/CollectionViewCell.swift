9b4a57463d69805bb5b548b5401b54a080f20662
matt johnston <mattjohnston@bokzatrix.com>
2015-11-26 11:09:38 -0500

    CollectionViewCell.swift

---
+++ saturday/saturday/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  saturday
+//
+//  Created by matt johnston on 11/26/15.
+//  Copyright © 2015 matt johnston. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+}
