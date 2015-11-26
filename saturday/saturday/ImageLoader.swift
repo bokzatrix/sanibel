c72b12f15eb6e6757abf01cedc67cd55060f6883
matt johnston <mattjohnston@bokzatrix.com>
2015-11-26 11:10:53 -0500

    ImageLoader.swift

---
+++ saturday/saturday/ImageLoader.swift
@@ -0,0 +1,14 @@
+//
+//  ImageLoader.swift
+//  saturday
+//
+//  Created by matt johnston on 11/26/15.
+//  Copyright © 2015 matt johnston. All rights reserved.
+//
+
+import Foundation
+
+struct ImageLoader {
+    let imageArray : [String] = ["Image-0", "Image-1", "Image-2", "Image-3","Image-4"]
+
+}
